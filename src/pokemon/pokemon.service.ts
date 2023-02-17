0c21c4f303d3e2f23ce4874a664bde61c7bd66a0
Matías Roy Rodriguez <matiasrr980@gmail.com>
2023-02-17 20:42:14 -0300

    feat: create pokemon in database

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,27 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose/dist';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
+
+	constructor(
+		@InjectModel(Pokemon.name)
+		private readonly pokemonModel: Model<Pokemon>
+	) { }
+
+	async create(createPokemonDto: CreatePokemonDto) {
 		createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
-    return createPokemonDto;
-  }
 
-  findAll() {
-    return `This action returns all pokemon`;
-  }
+		const pokemon = await this.pokemonModel.create( createPokemonDto );
+		return pokemon;
+	}
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
-  }
+	findAll() {
+		return `This action returns all pokemon`;
+	}
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
-  }
+	findOne(id: number) {
+		return `This action returns a #${id} pokemon`;
+	}
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
-  }
+	update(id: number, updatePokemonDto: UpdatePokemonDto) {
+		return `This action updates a #${id} pokemon`;
+	}
+
+	remove(id: number) {
+		return `This action removes a #${id} pokemon`;
+	}
 }
