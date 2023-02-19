e1bb28cb9984e5edc27ba8e5fb965935b4980945
Matías Roy Rodriguez <matiasrr980@gmail.com>
2023-02-18 23:01:45 -0300

    feat: create custom provider

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,27 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose/dist';
-import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
 
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { AxiosAdapter } from '../common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
 
 
-	private readonly axios: AxiosInstance = axios;
-
 	constructor(
 		@InjectModel(Pokemon.name)
-		private readonly pokemonModel: Model<Pokemon>
+		private readonly pokemonModel: Model<Pokemon>,
+
+		private readonly http:AxiosAdapter,
 	) { }
 
 	async executedSeed() {
 
 		await this.pokemonModel.deleteMany({});
 
-		const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+		const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
 
 		const pokemonToInsert: { name: string, no: number }[] = [];
 
